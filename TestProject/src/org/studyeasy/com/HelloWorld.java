7ee9f952cdc6ae2986ffd39f1c318394f9dcd87d
Sarvesh PC <Sarvesh PC@DESKTOP-4B1VRTQ>
2023-07-24 01:32:42 +0530

    First Test project

---
+++ TestProject/src/org/studyeasy/com/HelloWorld.java
@@ -0,0 +1,12 @@
+package org.studyeasy.com;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.print("Hello World");
+
+	}
+
+}
